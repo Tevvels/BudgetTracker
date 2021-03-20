4f8487f40073e40597313618e424010e56e62e3d
chris watkins <chrisbwatkins@gmail.com>
2021-03-19 18:57:22 -0600

    deploying again

--- public/db.js
+++ public/db.js
@@ -5,6 +5,7 @@ const request = indexedDB.open("budget",1);
 request.onupgradeneeded = function(e) {
     const db = e.target.result;
     db.createObjectStore("pending", { autoIncrement: true});
+    
 
 };
 
